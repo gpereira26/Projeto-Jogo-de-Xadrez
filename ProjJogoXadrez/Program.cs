b575887249a4a24cff9119e665545add2c4cafe0
Gustavo Pereira <gustavologin923@gmail.com>
2025-07-17 16:03:26 -0300

    Criando a classe posĩção

--- ProjJogoXadrez/Program.cs
+++ ProjJogoXadrez/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tabuleiro;
+namespace ProjJogoXadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Posicao P = new Posicao(3, 4);
+
+        Console.WriteLine("Posição " + P);
+
+            Console.ReadLine();
+        }
+    }
+}
