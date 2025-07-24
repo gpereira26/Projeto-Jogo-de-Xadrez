b0b83ef78aaab1e66827b60c3d320d9c5a84fe75
Gustavo Pereira <gustavologin923@gmail.com>
2025-07-24 11:12:00 -0300

    Fazendo os primeiros testes de movimentacao

--- ProjJogoXadrez/Program.cs
+++ ProjJogoXadrez/Program.cs
@@ -7,15 +7,22 @@ namespace ProjJogoXadrez
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
             try
             {
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 7));
-                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(1, 3));
-                tab.ColocarPeca(new Rei(tab, Cor.Branca), new Posicao(2, 4));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(6, 3));
-                Tela.ImprimirTabuleiro(tab);
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+                while (!partida.Terminada)
+                {
+                    Console.Clear();
+                    Tela.ImprimirTabuleiro(partida.Tab);
+
+                    Console.WriteLine();
+                    Console.WriteLine("Digite a Posição de Origem: ");
+                    Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+                    Console.Write("Digite a Posição de Destino: ");
+                    Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
+
+                    partida.ExecutaMovimento(origem, destino);
+                }
             }
             catch (TabuleiroException ex)
             {
--- ProjJogoXadrez/Tela.cs
+++ ProjJogoXadrez/Tela.cs
@@ -1,5 +1,6 @@
 ﻿using tabuleiro;
 using tabuleiro.Enums;
+using xadrez;
 
 namespace ProjJogoXadrez
 {
@@ -24,6 +25,13 @@ namespace ProjJogoXadrez
             Console.WriteLine("  a b c d e f g h");
         }
 
+        public static PosicaoXadrez LerPosicaoXadrez()
+        {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1] + "");
+            return new PosicaoXadrez(coluna, linha);
+        }
         public static void ImprimirPeca(Peca peca)
         {
             if (peca.Cor == Cor.Branca)
