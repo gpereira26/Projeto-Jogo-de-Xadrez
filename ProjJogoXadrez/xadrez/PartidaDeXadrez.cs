1f2264782f121ac05bc12db46dfa36ed248986f7
Gustavo Pereira <gustavologin923@gmail.com>
2025-07-31 13:39:37 -0300

    Jogada especial promocao

--- ProjJogoXadrez/xadrez/PartidaDeXadrez.cs
+++ ProjJogoXadrez/xadrez/PartidaDeXadrez.cs
@@ -132,6 +132,21 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = Tab.peca(destino);
+
+            // #jogadaespecial promocao 
+
+            if (p is Peao) 
+                if ((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+
+
             if (EstaEmXeque(Adversaria(JogadorAtual))) Xeque = true;
             else Xeque = false;
 
@@ -142,7 +157,6 @@ namespace xadrez
                 MudaJogador();
             }
 
-            Peca p = Tab.peca(destino);
 
             // #jogadaespecial en passant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2)) VulneravelEnPassant = p;
